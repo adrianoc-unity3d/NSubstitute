001d9846566ff958e910e22323c3c4363bb93121
Scott Bilas <scobi@unity3d.com>
2016-07-19 23:08:11 +0200

    new NSubstitute.Weaver.Tests.Fody project that exists solely to forward to NSubstitute.Weaver to make fody happy.
    also includes part 3 of rename of Weaving to Weavers because that's the convention fody wants, and might as well put everything in there instead.

---
+++ Source/Weavers/NSubstitute.Weaver.Tests.Fody/ModuleWeaver.cs
@@ -0,0 +1,20 @@
+﻿using Mono.Cecil;
+
+// this project exists just to make fody happy for in-solution weaving. note that this project
+// outputs to solution root "Weavers" folder for the binaries so fody picks it up in order to
+// patch the test project.
+//
+// https://github.com/Fody/Fody/wiki/InSolutionWeaving
+
+public class ModuleWeaver
+{
+    public ModuleDefinition ModuleDefinition { get; set; }
+
+    public void Execute()
+    {
+        var typeDefinition = new TypeDefinition(
+            GetType().Assembly.GetName().Name,
+            "TypeInjectedBy" + GetType().Name, TypeAttributes.Public, ModuleDefinition.Import(typeof(object)));
+        ModuleDefinition.Types.Add(typeDefinition);
+    }
+}
