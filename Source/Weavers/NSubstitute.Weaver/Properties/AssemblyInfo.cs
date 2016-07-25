ad415d3b7eac2442f5a88a6d4ad8952ff15f6ce6
Scott Bilas <scobi@unity3d.com>
2016-07-25 18:26:34 +0200

    properly generate the weavers.dll so fody will pick it up in the tests project
    also enabling access to call internals of the mock weaver. eventually we'll be building a fody module weaver directly into NSubstitute.Weaver and forwarding everything from our Tests.Fody to it.

--- Source/Weavers/NSubstitute.Weaver/Properties/AssemblyInfo.cs
+++ Source/Weavers/NSubstitute.Weaver/Properties/AssemblyInfo.cs
@@ -36,3 +36,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyFileVersion("1.0.0.0")]
 
 [assembly: InternalsVisibleTo("NSubstitute.Weaver.Tests")]
+[assembly: InternalsVisibleTo("Weavers")] // this is NSubstitute.Weaver.Tests.Fody
