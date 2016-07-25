be8bc19ba10132ce6f719c37a983bcb7791cb648
Scott Bilas <scobi@unity3d.com>
2016-07-25 17:02:06 +0200

    implement a test to ensure the fody injection basics are working

---
+++ Source/Weavers/NSubstitute.Weaver.Tests/FodyWeaver/FodyWeaverTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Reflection;
+using NUnit.Framework;
+using Shouldly;
+
+namespace NSubstitute.Weavers.Tests
+{
+    [TestFixture]
+    public class FodyWeaverTests
+    {
+        [Test]
+        public void Fody_Is_Patching_Test_Assembly()
+        {
+            Assembly.GetExecutingAssembly().GetType("Weavers.TypeInjectedByModuleWeaver").ShouldNotBeNull();
+        }
+    }
+}
