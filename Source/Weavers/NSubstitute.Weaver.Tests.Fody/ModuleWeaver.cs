fafde47bc8458e35192bca0c18ee06662e703e75
Scott Bilas <scobi@unity3d.com>
2016-07-26 10:45:01 +0200

    Updated in-solution fody-based test to depend on NSubstitute.dll and deliver that to the real weaver. Gets rid of the hard coded path specific to my machine.
    Also I'm just going to settle on the default name of ModuleWeaver for now, until we need otherwise. Perhaps if I turn this thing into a nuget package or something.

--- Source/Weavers/NSubstitute.Weaver.Tests.Fody/ModuleWeaver.cs
+++ Source/Weavers/NSubstitute.Weaver.Tests.Fody/ModuleWeaver.cs
@@ -1,6 +1,7 @@
 ﻿using System;
+using System.IO;
 using Mono.Cecil;
-using NSubstitute.Weavers;
+using RealModuleWeaver = NSubstitute.Weavers.Fody.ModuleWeaver;
 
 // this project exists just to make fody happy for in-solution weaving. note that this project
 // outputs to solution root "Weavers" folder for the binaries so fody picks it up in order to
@@ -10,16 +11,30 @@ using NSubstitute.Weavers;
 
 public class ModuleWeaver
 {
-    readonly FodyWeaver m_FodyWeaver = new FodyWeaver();
+    readonly RealModuleWeaver m_RealModuleWeaver = new RealModuleWeaver();
+    string m_AddinDirectoryPath;
 
-    public ModuleDefinition ModuleDefinition { get { return m_FodyWeaver.ModuleDefinition; } set { m_FodyWeaver.ModuleDefinition = value; } }
-    public Action<string> LogDebug { get { return m_FodyWeaver.LogDebug; } set { m_FodyWeaver.LogDebug = value; } }
-    public Action<string> LogInfo { get { return m_FodyWeaver.LogInfo; } set { m_FodyWeaver.LogInfo = value; } }
-    public Action<string> LogWarning { get { return m_FodyWeaver.LogWarning; } set { m_FodyWeaver.LogWarning = value; } }
-    public Action<string> LogError { get { return m_FodyWeaver.LogError; } set { m_FodyWeaver.LogError = value; } }
+    // see https://github.com/Fody/Fody/wiki/ModuleWeaver for all requirements and magic properties on this class
+
+    public ModuleDefinition ModuleDefinition { get { return m_RealModuleWeaver.ModuleDefinition; } set { m_RealModuleWeaver.ModuleDefinition = value; } }
+    public Action<string> LogDebug { get { return m_RealModuleWeaver.LogDebug; } set { m_RealModuleWeaver.LogDebug = value; } }
+    public Action<string> LogInfo { get { return m_RealModuleWeaver.LogInfo; } set { m_RealModuleWeaver.LogInfo = value; } }
+    public Action<string> LogWarning { get { return m_RealModuleWeaver.LogWarning; } set { m_RealModuleWeaver.LogWarning = value; } }
+    public Action<string> LogError { get { return m_RealModuleWeaver.LogError; } set { m_RealModuleWeaver.LogError = value; } }
+
+    // special: we have a hard coded copy-local msbuild ref to NSubstitute.dll specifically to give to the real weaver. needed to support in-solution weaver tests.
+    public string AddinDirectoryPath
+    {
+        get { return m_AddinDirectoryPath; }
+        set
+        {
+            m_AddinDirectoryPath = value;
+            m_RealModuleWeaver.NSubstituteAssemblyPath = Path.Combine(m_AddinDirectoryPath, "NSubstitute.dll");
+        }
+    }
 
     public void Execute()
     {
-        m_FodyWeaver.Execute();
+        m_RealModuleWeaver.Execute();
     }
 }
--- Source/Weavers/NSubstitute.Weaver/FodyWeaver/ModuleWeaver.cs
+++ Source/Weavers/NSubstitute.Weaver/FodyWeaver/ModuleWeaver.cs
@@ -1,14 +1,16 @@
 ﻿using System;
+using System.IO;
 using Mono.Cecil;
 using Unity.Cecil.Visitor;
+using TypeAttributes = Mono.Cecil.TypeAttributes;
 
-namespace NSubstitute.Weavers
+namespace NSubstitute.Weavers.Fody
 {
-    public class FodyWeaver
+    public class ModuleWeaver
     {
         // see https://github.com/Fody/Fody/wiki/ModuleWeaver for all requirements and magic properties on this class
 
-        // we're given the module to weave
+        // we're given the module to weave and the xml element for the FodyWeavers.xml spec
         public ModuleDefinition ModuleDefinition { get; set; }
 
         // filled by fody with delegates that will log to msbuild
@@ -17,12 +19,25 @@ namespace NSubstitute.Weavers
         public Action<string> LogWarning { get; set; }
         public Action<string> LogError { get; set; }
 
+        // may be filled by an NSubstitute wrapper task, if any (otherwise we autodetect location)
+        public string NSubstituteAssemblyPath { get; set; }
+
         // called via fody during msbuild
         public void Execute()
         {
-            const string nsubstituteAssemblyPath = @"C:\Proj\_external\NSubstitute\Output\Debug\NET35\NSubstitute\NSubstitute.dll";
+            if (NSubstituteAssemblyPath == null)
+            {
+                throw new NotImplementedException();
+            }
 
-            ModuleDefinition.Accept(new MockInjectorVisitor(AssemblyDefinition.ReadAssembly(nsubstituteAssemblyPath), ModuleDefinition));
+            if (!File.Exists(NSubstituteAssemblyPath))
+            {
+                throw new FileNotFoundException("Unable to weave without a valid NSubstitute assembly", NSubstituteAssemblyPath);
+            }
+
+            LogInfo("Using NSubstitute assembly: " + NSubstituteAssemblyPath);
+
+            ModuleDefinition.Accept(new MockInjectorVisitor(AssemblyDefinition.ReadAssembly(NSubstituteAssemblyPath), ModuleDefinition));
 
             // this is copied from a sample, but let's leave it in here to check basic fody injection mechanics are set up right (there's a test for it elsewhere)
             var typeDefinition = new TypeDefinition("NSubstitute.Weavers.Tests", "InjectedTypeForTest", TypeAttributes.NotPublic, ModuleDefinition.Import(typeof(object)));
