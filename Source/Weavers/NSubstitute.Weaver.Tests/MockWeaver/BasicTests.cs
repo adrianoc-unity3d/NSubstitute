924fdc9c392e8fce2ce9804e2cfa457d3a9489a8
Scott Bilas <scobi@unity3d.com>
2016-10-03 23:23:54 +0200

    new tests to demo some bugs we've got

---
+++ Source/Weavers/NSubstitute.Weaver.Tests/MockWeaver/BasicTests.cs
@@ -0,0 +1,36 @@
+﻿using NSubstitute.Exceptions;
+using NUnit.Framework;
+using Shouldly;
+
+namespace NSubstitute.Weaver.Tests
+{
+    [TestFixture]
+    class BasicTests
+    {
+        [Test]
+        public void Can_Mock_Class()
+        {
+            Substitute.For<ClassWithNoDefaultCtor>();
+        }
+
+        [Test]
+        public void Mocking_Class_With_Constructor_Params_Should_Throw()
+        {
+            Should.Throw<SubstituteException>(() => Substitute.For<ClassWithNoDefaultCtor>(null));
+            Should.Throw<SubstituteException>(() => Substitute.For<ClassWithNoDefaultCtor>("test"));
+            Should.Throw<SubstituteException>(() => Substitute.For<ClassWithNoDefaultCtor>(null, null));
+        }
+
+        [Test]
+        public void Can_Mock_Class_Containing_ICall_In_Ctor()
+        {
+            Substitute.For<ClassWithCtorICall>();
+        }
+
+        [Test]
+        public void Can_Mock_Class_Containing_Throw_In_Ctor()
+        {
+            Substitute.For<ClassWithCtorThrow>();
+        }
+    }
+}
---
+++ Source/Weavers/NSubstitute.Weaver.Tests/MockWeaver/BasicTypes.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace NSubstitute.Weaver.Tests
+{
+    class ClassWithNoDefaultCtor
+    {
+        public ClassWithNoDefaultCtor(string i) { }
+        public ClassWithNoDefaultCtor(string i1, string i2) { }
+    }
+
+    public class ClassWithCtorICall
+    {
+        public ClassWithCtorICall()
+	    {
+		    DoICall();
+	    }
+    
+        [MethodImpl((MethodImplOptions)0x1000)]
+        static extern void DoICall();
+    }
+
+    public class ClassWithCtorThrow
+    {
+        public ClassWithCtorThrow()
+	    {
+		    throw new InvalidOperationException();
+	    }
+    }
+}
